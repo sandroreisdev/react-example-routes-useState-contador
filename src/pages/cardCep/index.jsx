a47613eeabc9adf5c8a650271b670a407cc375e6
sandroreisdev <sandroreisdev@gmail.com>
2025-12-30 18:51:10 -0300

    agora mostrandos os dados na web

--- src/pages/cardCep/index.jsx
+++ src/pages/cardCep/index.jsx
@@ -1,24 +1,37 @@
 import { useState } from 'react'
 import './style.css'
 
-export const CardCep = () => {   
+export const CardCep = () => {
     const [cep, setCep] = useState()
+    const [logradouro, setLogradouro] = useState()
+    const [bairro, setBairro] = useState()
+    const [regiao, setRegiao] = useState()
+    const [estado, setEstado] = useState()
 
     const buscarCep = (e) => {
         e.preventDefault()
         fetch(`https://viacep.com.br/ws/${cep}/json/`)
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data)           
-        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data)
+                setLogradouro(data.logradouro)
+                setBairro(data.bairro)
+                setRegiao(data.regiao)
+                setEstado(data.estado)
+            })
     }
     return (
         <>
             <h1>Consulta Cep 🔎</h1>
             <form>
-                <input type="number" placeholder='Digite um CEP:'required value={cep} onChange={(e) => setCep(e.target.value)}/>
+                <input type="number" placeholder='Digite um CEP:' required value={cep} onChange={(e) => setCep(e.target.value)} />
                 <button type="submit" onClick={buscarCep}>Buscar</button>
-            </form>        
+            </form>
+
+            <p><strong>Logradouro: </strong><span>{logradouro}</span></p>
+            <p><strong>Bairro: </strong><span>{bairro}</span></p>
+            <p><strong>Regiao: </strong><span>{regiao}</span></p>
+            <p><strong>Estado: </strong><span>{estado}</span></p>
 
         </>
     )
